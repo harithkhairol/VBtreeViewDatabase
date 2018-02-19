42879f0faaefb500746339f736ea8e46ec8399e4
harithkhairol <harithkhairol@gmail.com>
2018-02-19 20:30:42 +0800

    initial commit

---
+++ treeViewDb4/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Testdb4DataSet1 = New treeViewDb4.testdb4DataSet1()
+        Me.EmployeesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.EmployeesTableAdapter = New treeViewDb4.testdb4DataSet1TableAdapters.employeesTableAdapter()
+        Me.TableAdapterManager = New treeViewDb4.testdb4DataSet1TableAdapters.TableAdapterManager()
+        Me.WorkingdaysTableAdapter = New treeViewDb4.testdb4DataSet1TableAdapters.workingdaysTableAdapter()
+        Me.WorkingdaysBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DGV = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.Testdb4DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WorkingdaysBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(55, 33)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(437, 572)
+        Me.TreeView1.TabIndex = 0
+        '
+        'Testdb4DataSet1
+        '
+        Me.Testdb4DataSet1.DataSetName = "testdb4DataSet1"
+        Me.Testdb4DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'EmployeesBindingSource
+        '
+        Me.EmployeesBindingSource.DataMember = "employees"
+        Me.EmployeesBindingSource.DataSource = Me.Testdb4DataSet1
+        '
+        'EmployeesTableAdapter
+        '
+        Me.EmployeesTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.employeesTableAdapter = Me.EmployeesTableAdapter
+        Me.TableAdapterManager.UpdateOrder = treeViewDb4.testdb4DataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.workingdaysTableAdapter = Me.WorkingdaysTableAdapter
+        '
+        'WorkingdaysTableAdapter
+        '
+        Me.WorkingdaysTableAdapter.ClearBeforeFill = True
+        '
+        'WorkingdaysBindingSource
+        '
+        Me.WorkingdaysBindingSource.DataMember = "workingdays"
+        Me.WorkingdaysBindingSource.DataSource = Me.Testdb4DataSet1
+        '
+        'DGV
+        '
+        Me.DGV.AllowUserToAddRows = False
+        Me.DGV.AllowUserToDeleteRows = False
+        Me.DGV.AutoGenerateColumns = False
+        Me.DGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2})
+        Me.DGV.DataSource = Me.WorkingdaysBindingSource
+        Me.DGV.Location = New System.Drawing.Point(527, 33)
+        Me.DGV.Name = "DGV"
+        Me.DGV.ReadOnly = True
+        Me.DGV.RowTemplate.Height = 28
+        Me.DGV.Size = New System.Drawing.Size(920, 572)
+        Me.DGV.TabIndex = 1
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "node"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "node"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        Me.DataGridViewTextBoxColumn1.Visible = False
+        Me.DataGridViewTextBoxColumn1.Width = 70
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "content"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "content"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.ReadOnly = True
+        Me.DataGridViewTextBoxColumn2.Width = 88
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(1643, 682)
+        Me.Controls.Add(Me.DGV)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Testdb4DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WorkingdaysBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents Testdb4DataSet1 As treeViewDb4.testdb4DataSet1
+    Friend WithEvents EmployeesBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents EmployeesTableAdapter As treeViewDb4.testdb4DataSet1TableAdapters.employeesTableAdapter
+    Friend WithEvents TableAdapterManager As treeViewDb4.testdb4DataSet1TableAdapters.TableAdapterManager
+    Friend WithEvents WorkingdaysTableAdapter As treeViewDb4.testdb4DataSet1TableAdapters.workingdaysTableAdapter
+    Friend WithEvents WorkingdaysBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents DGV As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
+
+End Class
---
+++ treeViewDb4/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Testdb4DataSet1.workingdays' table. You can move, or remove it, as needed.
+        Me.WorkingdaysTableAdapter.Fill(Me.Testdb4DataSet1.workingdays)
+        'TODO: This line of code loads data into the 'Testdb4DataSet1.employees' table. You can move, or remove it, as needed.
+        Me.EmployeesTableAdapter.Fill(Me.Testdb4DataSet1.employees)
+
+
+
+        EmployeesTableAdapter.Connection.Open()
+        '   TreeView1.Nodes.Clear()
+        FillTree("1", "Level", Nothing)
+        EmployeesTableAdapter.Connection.Close()
+
+
+    End Sub
+
+    Public Sub FillTree(ByVal Key As String, ByVal Txt As String, ByVal N As TreeNode)
+
+        Dim cn As OleDbConnection
+        Dim cmd As OleDbCommand
+        Dim NN As TreeNode
+
+        'if N has no parent than N is parent
+
+        If N Is Nothing Then
+            NN = TreeView1.Nodes.Add(Key, Txt)
+
+            'if N has parent than it become child node
+        Else
+            NN = N.Nodes.Add(Key, Txt)
+        End If
+
+        cn = EmployeesTableAdapter.Connection()
+        cmd = New OleDbCommand("select * from employees where node_parent='" & Key & "'", cn)
+
+        Dim dr = cmd.ExecuteReader
+        Do While dr.Read()
+            FillTree(dr("node"), dr("node_name"), NN)
+        Loop
+
+        dr.Close()
+        cmd.Dispose()
+
+    End Sub
+
+    Private Sub EmployeesBindingNavigatorSaveItem_Click(sender As System.Object, e As System.EventArgs)
+        Me.Validate()
+        Me.EmployeesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Testdb4DataSet1)
+
+    End Sub
+
+    Private Sub TreeView1_AfterSelect(sender As System.Object, e As System.Windows.Forms.TreeViewEventArgs) Handles TreeView1.AfterSelect
+
+        If TreeView1.SelectedNode Is Nothing Then
+
+            DGV.DataSource = Nothing
+
+            Exit Sub
+
+        End If
+
+        If TreeView1.SelectedNode.Name = "root" Then
+
+            DGV.DataSource = Nothing
+            Exit Sub
+
+        End If
+
+        Dim ID As String
+        ID = TreeView1.SelectedNode.Name
+
+
+
+
+        DGV.DataSource = Testdb4DataSet1.workingdays.Select("node='" & ID & "'")
+
+
+
+
+
+        DGV.Visible = True
+
+
+
+
+
+
+    End Sub
+
+    Private Sub WorkingdaysDataGridView_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DGV.CellContentClick
+
+    End Sub
+End Class
